5e4382a92ec63c86993c9efb8d26d491e727cbb4
jbeedle19 <josh.beedle@gmail.com>
2021-02-03 15:15:51 -0500

    created Comment API CRUD routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,3 +1,48 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+router.post('/', /* withAuth, */ (req, res) => {
+    // Expects: {comment_text: 'This is the comment', user_id: 1, post_id: 1}
+    Comment.create({
+        comment_text: req.body.comment_text,
+        post_id: req.body.post_id,
+        // Change to req.session.user_id once login is complete
+        user_id: req.body.user_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+
+router.delete('/:id', /* withAuth, */ (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment with this id!' });
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
